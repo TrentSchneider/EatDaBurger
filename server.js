6240279c79ddaedb1b29885b87ddf76832151c12
Trent Schneider <trentonschneider@gmail.com>
2020-08-20 14:01:28 -0400

    set up base handlebars

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-var express = require("express");
-var exphbs = require("express-handlebars");
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mysql = require("mysql");
+const DB_PW = require("./db_pw");
 
-var app = express();
+const app = express();
 
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// TODO swap out custom helper with if (checks truthy) and unless (checks falsy) see index.handlebars to swap out handlebars reference
+// TODO Scott had issues inserting the if/unless into an each loop, so verify if that is possible
+// TODO add the additional file plaths from the readme and distribute everthing to the designated directories
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
@@ -26,12 +34,12 @@ connection.connect(function (err) {
 });
 
 app.get("/", function (req, res) {
-  connection.query("SELECT * FROM burgers;", function (err, data) {
+  connection.query("SELECT * FROM burger", function (err, data) {
     if (err) {
       return res.status(500).end();
     }
-
-    res.render("index", { plans: data });
+    console.log(data);
+    res.render("index", { burger: data });
   });
 });
 
