45225aa81aebd6508f2ca13768c613c79d3ec9b0
Trent Schneider <trentonschneider@gmail.com>
2020-08-20 15:21:20 -0400

    added schema and seeds

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS burger_db;
+CREATE DATABASE burger_db;
+USE burger_db;
+
+CREATE TABLE burger (
+id INT NOT NULL AUTO_INCREMENT,
+burger VARCHAR(30) NOT NULL,
+devoured BOOLEAN DEFAULT false,
+PRIMARY KEY(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burger (burger)
+VALUES ("Cheeseburger");
+INSERT INTO burger (burger, devoured)
+VALUES ("Bacon Burger", true);
+INSERT INTO burger (burger)
+VALUES ("Mushroom Burger");
+INSERT INTO burger (burger, devoured)
+VALUES ("Steak Burger", true);
