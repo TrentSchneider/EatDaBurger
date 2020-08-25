dda7445ea4f0d6b0f5fb6cb870be185601191960
Trent Schneider <trentonschneider@gmail.com>
2020-08-24 23:19:28 -0400

    testing solution for heroku issue

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,17 @@
 const mysql = require("mysql");
 // const DB_PW = require("../../db_pw");
 let connection;
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: DB_PW,
-    database: "burger_db",
-  });
-}
+// if (process.env.JAWSDB_URL) {
+connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: DB_PW,
+//     database: "burger_db",
+//   });
+// }
 
 connection.connect(function (err) {
   if (err) {
